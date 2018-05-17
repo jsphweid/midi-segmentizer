3bb0d8e34076e61eebfcefe3407b51f685940284
Joseph Weidinger <jsphweid@gmail.com>
2018-05-17 00:51:01 -0500

    export types and module

--- src/main-module.ts
+++ src/main-module.ts
@@ -1,7 +1,8 @@
 import ArgumentValidator from './program-argument-validator'
 import { processMidiFile } from './helpers'
+import { SegmentInfoType } from './types'
 
-export function segmentizeMidi(midiFile: string) {
+export function segmentizeMidi(midiFile: string): SegmentInfoType[] {
 	const hasValidArguments = ArgumentValidator.hasValidArguments(midiFile)
 	return hasValidArguments ? processMidiFile(midiFile) : null
 }
